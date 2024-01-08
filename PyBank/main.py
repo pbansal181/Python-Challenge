b3af4a4b42efb958961f177950ac061b6c7a2502
pbansal181 <pbansal181@gmail.com>
2024-01-08 16:30:15 -0500

    final Changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,7 +9,7 @@ text_file = os.path.join('Analysis', 'Financial_summary.txt')
 #create empty list variables
 total_month = []
 total_profit = []
-profit_change_month = []
+profit_change = []
 
 with open(csvpath) as csvfile:
  csvreader = csv.reader(csvfile, delimiter = ',')
@@ -21,22 +21,22 @@ with open(csvpath) as csvfile:
 
 #iterate through the profits to find changes in profit/loss
  for i in range(len(total_profit) - 1):
-    profit_change_month.append(total_profit[i+1] - total_profit[i])
+    profit_change.append(total_profit[i+1] - total_profit[i])
 
 #Calculation of max and min profit change
-    max_increase = max(profit_change_month)
-    max_decrease = min(profit_change_month)
+    max_increase = max(profit_change)
+    max_decrease = min(profit_change)
 
 #Final value of max incraese and decrease monthly
-    max_inc_month = profit_change_month.index(max(profit_change_month)) + 1
-    max_dec_month = profit_change_month.index(min(profit_change_month)) + 1
+    max_inc_month = profit_change.index(max(profit_change)) + 1
+    max_dec_month = profit_change.index(min(profit_change)) + 1
 
 #Printing the output statements
     print("Financial Analysis")
     print("---------------------------------")
     print(f"Total Months: {len(total_month)}")
     print(f"Total: ${sum(total_profit)}")
-    print(f"Average Change: ${round(sum(profit_change_month)/len(profit_change_month),2)}")
+    print(f"Average Change: ${round(sum(profit_change)/len(profit_change),2)}")
     print(f"Greatest Increase in Profits: {total_month[max_inc_month]} (${(str(max_increase))})")
     print(f"Greatest Decrease in Profits: {total_month[max_dec_month]} (${(str(max_decrease))})")
 
@@ -46,7 +46,7 @@ with open(csvpath) as csvfile:
         file.write("----------------------------\n")
         file.write(f"Total Months: {len(total_month)}\n")
         file.write(f"Total: ${sum(total_profit)}\n")
-        file.write(f"Average Change: ${round(sum(profit_change_month)/len(profit_change_month),2)}\n")
+        file.write(f"Average Change: ${round(sum(profit_change)/len(profit_change),2)}\n")
         file.write(f"Greatest Increase in Profits: {total_month[max_inc_month]} (${(str(max_increase))})\n")
         file.write(f"Greatest Decrease in Profits: {total_month[max_dec_month]} (${(str(max_decrease))})\n")
 
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Python-Challenge
+In this Challenge we need to create 2 python scripts for calculating Financial and Election summary for data given.
+There are 2 Files to be created:
+PyBank Details
+In this data there are 2 columns named as "Date" and "Profit/Losses".
+The task is calculating the below values:
+•The total number of months included in the dataset(This can be calculated using a list and adding 1 everytime as we iterate through all rows when we find a new value and then getting the length of it)
+•The net total amount of "Profit/Losses" over the entire period(This is calculated by adding the value of 2nd column to a list variable as we iterate through row and finally getting sum of it)
+•The changes in "Profit/Losses" over the entire period, and then the average of those changes(Change can be find out going in loop and getting difference of 1st and next entry. Average can be find out as divison of summing and length of the changes)
+•The greatest increase in profits (date and amount) over the entire period(Can be find out using max function)
+•The greatest decrease in profits (date and amount) over the entire period(can be found out using min function)
+
+PyPoll Details
+This dataset is composed of three columns: "Voter ID", "County", and "Candidate"
+The task is to calculate below values:
+•The total number of votes cast(It can be calculated by getting length of the column)
+•A complete list of candidates who received votes(creating a list with counting candidate from 3rd column) 	
+•The percentage of votes each candidate won(Percentage can be calculated using formula [candidate’s vote/total votes] *100)
+•The total number of votes each candidate won( putting a counter for each candidate and then counting occurence)
+•The winner of the election based on popular vote(For this we need to sort the candidate’s list in descending order and then fetching the first entry)
